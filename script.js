8afc8762b3f732dae437d608da1e7bb4210c8bae
Francoisxr030303 <124201106+Francoisxr030303@users.noreply.github.com>
2024-02-06 23:05:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("login-form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe por defecto
+    alert("¡Inicio de sesión exitoso!"); // Simplemente muestra un mensaje de alerta por ahora
+});
